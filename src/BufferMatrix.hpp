2fd1caf9c42468130fae3ca1940eec06b2279f64
Tigran <tigranateyan2009@gmail.com>
2025-06-19 19:20:30 +0400

    Update BufferMatrix.hpp

--- src/BufferMatrix.hpp
+++ src/BufferMatrix.hpp
@@ -39,6 +39,54 @@ public:
         other.cols = 0;
     }
 
+    BufferMatrix& operator=(const BufferMatrix& other)
+    {
+        if (this != &other)
+        {
+            if (buffer)
+            {
+                for (int i = 0; i < cols; ++i)
+                    delete[] buffer[i];
+                delete[] buffer;
+            }
+
+            rows = other.rows;
+            cols = other.cols;
+            buffer = new T*[cols];
+            for (int i = 0; i < cols; ++i)
+            {
+                buffer[i] = new T[rows];
+                for (int j = 0; j < rows; ++j)
+                    buffer[i][j] = other.buffer[i][j];
+            }
+        }
+        return *this;
+    }
+
+
+    BufferMatrix& operator=(BufferMatrix&& other) noexcept
+    {
+        if (this != &other)
+        {
+            if (buffer)
+            {
+                for (int i = 0; i < cols; ++i)
+                    delete[] buffer[i];
+                delete[] buffer;
+            }
+
+            buffer = other.buffer;
+            rows = other.rows;
+            cols = other.cols;
+
+            other.buffer = nullptr;
+            other.rows = 0;
+            other.cols = 0;
+        }
+        return *this;
+    }
+
+
     ~BufferMatrix()
     {
         if (buffer)
